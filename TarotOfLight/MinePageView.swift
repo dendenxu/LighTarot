cb43bdc07c88e1d1c56bdbd5a26fd469e0f90ed9
xzdd@xzdds-MBP <56295612@qq.com>
2020-10-09 10:35:09 +0800

    to make button OK

--- TarotOfLight/MinePageView.swift
+++ TarotOfLight/MinePageView.swift
@@ -12,57 +12,76 @@ import SDWebImageSwiftUI
 
 struct MinePageView: View {
     @State var tudouAnimating = true
+    @State var isChangingAvatar = false
+    var otherOpacity: Double {
+        get {
+            return isChangingAvatar ? 0.25 : 1
+        }
+    }
+    var avatarScale: CGFloat = 1.25
+    @State var image: Image? = Image("head")
+    @State var showImagePicker: Bool = false
     var body: some View {
         VStack {
-//            HStack {
-//                Spacer()
-//                Image("edit")
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(width: 40)
-//            }.padding(.trailing, 30)
-//                .padding(.bottom, 100)
-//                .padding(.top, 50)
-
-            ZStack {
-                WebImage(
-                    url: URL(fileURLWithPath: Bundle.main.path(forResource: "cute", ofType: "gif") ?? "cute.gif"),
-                    isAnimating: self.$tudouAnimating)
-                    .resizable()
-                    .playbackRate(1.0)
-                    .retryOnAppear(true)
+            VStack {
+                Button(action: {
+                    self.showImagePicker.toggle()
+                    print("Toggled!")
+                }) {
+                    //            WebImage(
+                    //                url: URL(fileURLWithPath: Bundle.main.path(forResource: "cute", ofType: "gif") ?? "cute.gif"),
+                    //                isAnimating: self.$tudouAnimating)
+                    image?
+                        .resizable()
+                    //                .playbackRate(1.0)
+                    //                .retryOnAppear(true)
                     .scaledToFit()
-                    .frame(width: 300, height: 300)
-                    .clipShape(Circle())
-                    .shadow(radius: 10)
-                    .background(
-                        ShinyBackground(
-                            nStroke: 30, nFill: 15,
-                            size: CGSize(
-                                width: UIScreen.main.bounds.width,
-                                height: UIScreen.main.bounds.height
-                            )
-                        ).opacity(0.5)
-                    )
-                    .padding(.top, 90)
-            }
+                        .frame(width: 250, height: 250)
+                        .clipShape(Circle())
+                        .shadow(radius: 10)
+                        .background(
+                            ShinyBackground(
+                                nStroke: 30, nFill: 15,
+                                size: CGSize(
+                                    width: UIScreen.main.bounds.width,
+                                    height: UIScreen.main.bounds.height
+                                )
+                            ).opacity(0.5)
+                        )
+                        .onLongPressGesture(perform: {
+                            print("Should implement changing avatar")
+                            //                    withAnimation(springAnimation) {
+                            //                        isChangingAvatar = true
+                            //                    }
 
+                        })
+                        .scaleEffect(isChangingAvatar ? avatarScale : 1)
+                        .zIndex(1)
+                        .padding(.top, 150)
+                }
+            }
+                .sheet(isPresented: $showImagePicker) {
+                    ImagePicker(sourceType: .photoLibrary) { image in
+                        self.image = Image(uiImage: image)
+                    }
+            }
             Spacer()
 
+            VStack {
+                ShinyText(text: "小土豆", font: "DFPHeiW12-GB", size: 30, textColor: Color.black.opacity(0.75), shadowColor: Color.black.opacity(0.3))
+                    .padding(.top, 10)
+                textField(text: "出生时间", placeholder: "出生时间", imageName: "time")
+                    .padding(.top, 30)
 
-            ShinyText(text: "小土豆", font: "DFPHeiW12-GB", size: 30, textColor: Color.black.opacity(0.75), shadowColor: Color.black.opacity(0.3))
-                .padding(.top, 10)
-            textField(text: "出生时间", placeholder: "出生时间", imageName: "time")
-                .padding(.top, 30)
+                textField(text: "现居地", placeholder: "现居地", imageName: "location")
+                    .padding(.top, 30)
 
-            textField(text: "现居地", placeholder: "现居地", imageName: "location")
-                .padding(.top, 30)
-
-            Text("资料完整度越高，占卜越真实哦！")
-                .font(.custom("Source Han Sans Medium", size: 15))
-                .foregroundColor(Color.black.opacity(0.4))
-                .padding(.top, 30)
-                .padding(.bottom, 200)
+                Text("资料完整度越高，占卜越真实哦！")
+                    .font(.custom("Source Han Sans Medium", size: 15))
+                    .foregroundColor(Color.black.opacity(0.4))
+                    .padding(.top, 30)
+                    .padding(.bottom, 200)
+            }.opacity(otherOpacity)
 
         }
     }
