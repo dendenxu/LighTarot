4847153af861fd6f277f1180d772b1a985f474f2
xzdd@xzdds-MBP <56295612@qq.com>
2020-10-09 23:22:15 +0800

    update

--- InterpreterView.swift
+++ InterpreterView.swift
@@ -84,7 +84,7 @@ struct CheckedLazyVStack<Content: View>: View {
 }
 
 struct InterpreterView: View {
-    var cards = [CardInfo(imageName: "theHierophant", interpretText: "    援助、同情、宽宏大量，可信任的人给予的劝告，良好的商量对象，得到精神上的满足，遵守规则。\n\n    爱情上屈从于他人的压力，只会按照对方的要求来盲目改变自己，自以为这是必要的付出，其实不过是被迫的选择。伴侣也不会对你保持忠诚，并很难满足双方真实的需要。", storyText: "    这是一张代表唤醒良心与善良觉醒的牌，同时也是张关于宗教信仰与传统的牌。\n    同皇帝所代表的物质主宰相比，他更趋向于精神层面，他是精神方面的权威，是未知世界的解释者。因为教皇能够直接与上帝联系，他用慈悲与洞察力试图拯救世人的灵魂，并用自己的言论引导人们走向正途。\n    牌面中的教皇高举双手向世人传播教义，信徒们虔诚地跪在地上聆听他的教诲。然而需要注意的是他同时也是传统知识和保守道德的代表，他控制人们的思维，使人的眼界变得狭小。只有彻底放弃陈旧的一切，探索新的解决方式，可能还有希望。")]
+    var cards = [CardInfo(imageName: "theHierophant", interpretText: "    援助、同情、宽宏大量，可信任的人给予的劝告，良好的商量对象，得到精神上的满足，遵守规则。\n\n    爱情上屈从于他人的压力，只会按照对方的要求来盲目改变自己，自以为这是必要的付出，其实不过是被迫的选择。伴侣也不会对你保持忠诚，并很难满足双方真实的需要。", storyText: "    这是一张代表唤醒良心与善良觉醒的牌，同时也是张关于宗教信仰与传统的牌。\n\n    同皇帝所代表的物质主宰相比，他更趋向于精神层面，他是精神方面的权威，是未知世界的解释者。因为教皇能够直接与上帝联系，他用慈悲与洞察力试图拯救世人的灵魂，并用自己的言论引导人们走向正途。\n\n    牌面中的教皇高举双手向世人传播教义，信徒们虔诚地跪在地上聆听他的教诲。然而需要注意的是他同时也是传统知识和保守道德的代表，他控制人们的思维，使人的眼界变得狭小。只有彻底放弃陈旧的一切，探索新的解决方式，可能还有希望。")]
     var body: some View {
         VStack {
             ZStack {
@@ -118,15 +118,19 @@ struct InterpreterView: View {
                         .padding(.top, 10)
                         .zIndex(1)
                     ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .opacity(0.35)
-                            .foregroundColor(Color(hex: 0xaaaaaa))
-                            .frame(width: 180, height: 50)
-                            .shadow(color: Color.black.opacity(0.95), radius: 2)
                         VStack(spacing: 5) {
-                            ShinyText(text: "教皇", font: .DefaultChineseFont, size: 16, textColor: Color("StrangePurple"), shadowColor: Color.black.opacity(0))
-                            ShinyText(text: "(正位)", font: "Source Han Sans Heavy", size: 13, textColor: Color("StrangePurple"), shadowColor: Color.black.opacity(0))
-                        }.opacity(0.9)
+                            Spacer().frame(width: 1, height: 4)
+                            ShinyText(text: "教皇", font: .DefaultChineseFont, size: 20, textColor: Color("StrangePurple"), shadowColor: Color.black.opacity(0))
+                            ShinyText(text: "(正位)", font: "Source Han Sans Heavy", size: 16, textColor: Color("StrangePurple"), shadowColor: Color.black.opacity(0))
+                            Spacer().frame(width: 1, height: 4)
+                        }
+                            .background(RoundedRectangle(cornerRadius: 10)
+                                .opacity(0.35)
+                                .foregroundColor(Color(hex: 0xaaaaaa))
+                                .shadow(color: Color.black.opacity(0.95), radius: 2)
+                                .frame(width: 180)
+//                                .scaledToFill()
+                            )
                     }.background(ShinyBackground(
                         nStroke: 20,
                         nFill: 20,
@@ -139,10 +143,12 @@ struct InterpreterView: View {
                         .padding(.bottom, 25)
                         .zIndex(-0.5)
 
-                    CheckedLazyVStack(spacing: 60) {
-                        WrapComb(title: "释义解读", text: cards[0].interpretText)
-                        WrapComb(title: "牌面故事", text: cards[0].storyText)
-                    }
+
+                    WrapComb(title: "释义解读", text: cards[0].interpretText)
+                        .padding(.bottom, 30)
+                    WrapComb(title: "牌面故事", text: cards[0].storyText)
+                        .padding(.bottom, 30)
+
 
                     Spacer()
                 }
@@ -153,21 +159,22 @@ struct InterpreterView: View {
 
 
 struct WrapComb: View {
-    var title = "释义解读"
-    var text = "    援助、同情、宽宏大量，可信任的人给予的劝告，良好的商量对象，得到精神上的满足，遵守规则。\n\n    爱情上屈从于他人的压力，只会按照对方的要求来盲目改变自己，自以为这是必要的付出，其实不过是被迫的选择。伴侣也不会对你保持忠诚，并很难满足双方真实的需要。"
+    var title = "TITLE"
+    var text = "Some long long long text"
     var body: some View {
-        VStack {
+        VStack(alignment: .center, spacing: 5) {
             HStack {
                 Image("star")
                     .renderingMode(.original)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 30)
-                ShinyText(text: title, font: "Source Han Sans Heavy", size: 20, textColor: Color(hex: 0xF7EB2E), shadowColor: Color.black.opacity(0))
+                ShinyText(text: title, font: .DefaultChineseFont, size: 20, textColor: Color(hex: 0xF7EB2E), shadowColor: Color.black.opacity(0))
             }
                 .offset(x: -15)
+                .padding(.bottom, 10)
 //                .shadow(color: Color.black.opacity(0.6), radius: 3)
-            WrapText(text: text).padding(.top, 50)
+            WrapText(text: text)
         }
     }
 }
@@ -175,17 +182,19 @@ struct WrapComb: View {
 struct WrapText: View {
     var text = "Placeholder Text"
     var body: some View {
-        Text(text)
-            .font(Font.custom("Source Han Sans Medium", size: 14.5))
-            .lineSpacing(5)
-            .foregroundColor(.white)
-            .frame(width: 260)
+        VStack {
+            Spacer().frame(width: 1, height: 30)
+            Text(text)
+                .font(Font.custom("Source Han Sans Heavy", size: 14.5))
+                .lineSpacing(2)
+                .foregroundColor(.white)
+                .frame(width: 260)
+            Spacer().frame(width: 1, height: 30)
+        }
             .background(RoundedRectangle(cornerRadius: 10)
                 .opacity(0.25)
                 .foregroundColor(.white)
-//                .shadow(color: Color.black.opacity(0.95), radius: 5)
-            // TODO: HEADACHE, how to implement this?
-            .scaleEffect(x: 1.2, y: 1.3, anchor: .center)
+                .scaleEffect(x: 1.2, y: 1, anchor: .center)
             )
     }
 }
