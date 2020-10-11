690fa1ffbbeff82e327960a4d5232e11df8b9c8a
xzdd@xzdds-MBP <56295612@qq.com>
2020-10-11 14:10:53 +0800

    emm

---
+++ server/dotnet/beginner/helloworld.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace myApp {
+    class Program {
+        static void main () {
+            Console.WriteLine ("Hello, world!");
+        }
+    }
+}
